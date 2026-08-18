1e3aca4b7de6a2a910fd077938197ac49a6ac717
matiasb123 <matiasjbory@gmail.com>
2026-08-18 17:30:38 -0300

    Actualizar datos de clientes y reseñas

--- src/data/clients.ts
+++ src/data/clients.ts
@@ -1,24 +1,56 @@
 export type Client = {
   name: string;
-  asset?: string;
+  asset: string;
+  darkBackground?: boolean;
 };
 
-// Keep this list as the single place to add, remove, reorder, or replace client assets.
+// Approved client identity files are kept in public/clients. Casino Buenos Aires is
+// included following the client's explicit approval; Roemmers remains excluded pending review.
 export const clients: Client[] = [
   { name: "Facebook", asset: "/clients/facebook.svg" },
-  { name: "Mercado Libre", asset: "/clients/mercadolibre.svg" }, { name: "McDonald's", asset: "/clients/mcdonalds.svg" }, { name: "Shell", asset: "/clients/shell.svg" },
-  { name: "Toyota", asset: "/clients/toyota.svg" }, { name: "Tenaris", asset: "/clients/tenaris.png" }, { name: "Telecom", asset: "/clients/telecom.svg" }, { name: "DHL", asset: "/clients/dhl.svg" },
-  { name: "Farmacity", asset: "/clients/farmacity.png" }, { name: "HSBC", asset: "/clients/hsbc.svg" }, { name: "ICBC", asset: "/clients/icbc.svg" }, { name: "Bank of China" },
-  { name: "Volvo", asset: "/clients/volvo.svg" }, { name: "Scania", asset: "/clients/scania.svg" }, { name: "Quilmes", asset: "/clients/quilmes.png" }, { name: "Easy" },
-  { name: "Falabella" }, { name: "La Caja" }, { name: "Banco Provincia" }, { name: "Banco Ciudad" }, { name: "Banco Patagonia" },
-  { name: "Bunge" }, { name: "Baker Hughes" }, { name: "AES" }, { name: "RE/MAX" }, { name: "Grant Thornton Argentina" },
-  { name: "AppsFlyer" }, { name: "AVEVA" }, { name: "Roemmers" }, { name: "Hospital Italiano" }, { name: "Brinks" },
-  { name: "Colonia Express" }, { name: "Laboratorios Servier" }, { name: "Sartorius", asset: "/clients/sartorius.svg" },
-  { name: "Thermomix" }, { name: "Casino Buenos Aires" }, { name: "SUTERH" },
+  { name: "Mercado Libre", asset: "/clients/mercado-libre.svg" },
+  { name: "McDonald's", asset: "/clients/mcdonalds.svg" },
+  { name: "Shell", asset: "/clients/shell.svg" },
+  { name: "Toyota", asset: "/clients/toyota.svg" },
+  { name: "Tenaris", asset: "/clients/tenaris.svg" },
+  { name: "Telecom", asset: "/clients/telecom.svg" },
+  { name: "DHL", asset: "/clients/dhl.svg" },
+  { name: "Farmacity", asset: "/clients/farmacity.svg" },
+  { name: "HSBC", asset: "/clients/hsbc.svg" },
+  { name: "ICBC", asset: "/clients/icbc.svg" },
+  { name: "Bank of China", asset: "/clients/bank-of-china.png" },
+  { name: "Volvo", asset: "/clients/volvo.svg" },
+  { name: "Scania", asset: "/clients/scania.svg" },
+  { name: "Quilmes", asset: "/clients/quilmes-color.png" },
+  { name: "Easy", asset: "/clients/easy.svg" },
+  { name: "Falabella", asset: "/clients/falabella.svg" },
+  { name: "La Caja", asset: "/clients/la-caja.png" },
+  { name: "Banco Provincia", asset: "/clients/banco-provincia.svg" },
+  { name: "Banco Ciudad", asset: "/clients/banco-ciudad.svg" },
+  { name: "Banco Patagonia", asset: "/clients/banco-patagonia.svg" },
+  { name: "Bunge", asset: "/clients/bunge.svg" },
+  { name: "Baker Hughes", asset: "/clients/baker-hughes.svg" },
+  { name: "AES", asset: "/clients/aes.svg" },
+  { name: "RE/MAX", asset: "/clients/remax.svg" },
+  { name: "Grant Thornton Argentina", asset: "/clients/grant-thornton-argentina.png" },
+  { name: "AppsFlyer", asset: "/clients/appsflyer.svg" },
+  { name: "AVEVA", asset: "/clients/aveva.svg" },
+  { name: "Hospital Italiano", asset: "/clients/hospital-italiano.svg" },
+  { name: "Brinks", asset: "/clients/brinks.svg" },
+  { name: "Colonia Express", asset: "/clients/colonia-express-color.png" },
+  { name: "Laboratorios Servier", asset: "/clients/laboratorios-servier.svg" },
+  { name: "Sartorius", asset: "/clients/sartorius.svg" },
+  { name: "Thermomix", asset: "/clients/thermomix.svg" },
+  { name: "Casino Buenos Aires", asset: "/clients/casino-buenos-aires.png" },
+  { name: "SUTERH", asset: "/clients/suterh.png" },
 ];
 
 export const featuredClientNames = [
-  "Facebook", "Mercado Libre", "McDonald's", "Toyota", "Shell", "DHL", "Tenaris", "Farmacity", "HSBC", "Quilmes", "Telecom", "ICBC",
+  "Facebook", "Mercado Libre", "McDonald's", "Toyota",
+  "Shell", "DHL", "Tenaris", "Farmacity",
+  "HSBC", "Quilmes", "Telecom", "ICBC",
 ];
 
-export const marqueeClientNames = ["Volvo", "Scania", "Sartorius"];
+export const marqueeClientNames = clients
+  .map((client) => client.name)
+  .filter((name) => !featuredClientNames.includes(name));
